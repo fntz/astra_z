74df9b8124ea1e4ad09e2a0413875d67babb0bb6
fntzr <fantazuor@gmail.com>
2013-07-21 20:05:49 +0300

    add spec for translater

---
+++ test/spec/astra_z_translater_spec.js
@@ -0,0 +1,62 @@
+var c = function(m){console.log(m)};
+
+describe("Translater", function() {
+
+  it ("translate elements", function() {
+    var z = "(div(div)(span#span1)(span.class1.class2(div#id1.class1)))";
+
+    var t = Translater.translate(z);
+    var e = t.element;
+
+    expect(e).toHaveClass(Translater.default_class_name);
+    expect(e.down('span')).toHaveId('span1');
+    expect(e.down('span').next()).toHaveClass('class1');
+    expect(e.down('span').next()).toHaveClass('class2');
+    expect(e.down('span').next().down()).toHaveClass('class1');
+    expect(e.down('span').next().down()).toHaveId('id1');
+    expect(e.descendants().size()).toEqual(5);
+
+  });
+
+  it ("error when after `(` follow not tag", function() {
+    var z = "(#)";
+    expect(
+      function() { Translater.translate(z) }
+    ).toThrow();
+  });
+
+  it ("2 world", function() {
+    var z = "(div div)";
+    expect(
+      function() { Translater.translate(z) }
+    ).toThrow();
+  });
+
+  it ("unexpected class", function() {
+    var z = "(div.#)";
+    expect(
+      function() { Translater.translate(z) }
+    ).toThrow();
+  }); 
+
+  it ("unexpected id", function() {
+    var z = "(div#@)";
+    expect(
+      function() { Translater.translate(z) }
+    ).toThrow();
+  }); 
+
+  
+
+
+});
+
+
+
+
+
+
+
+
+
+
