7b1ede359aad131d22d16bdb7048861975de5aa3
fntzr <fantazuor@gmail.com>
2013-07-21 18:34:15 +0300

    add delegate specs

---
+++ test/spec/astra_z_util_spec.js
@@ -0,0 +1,56 @@
+
+
+describe("Delegatable", function() {
+  it("delegate", function() {
+    var C = Class.create(Delegatable, {
+      initialize: function(arr) {
+        this.arr = arr;
+        this.delegate('arr', 'size', 'first', 'uniq');
+      }
+    });
+
+    var z = new C([1,2,3,1]);
+
+    expect(z.size()).toEqual(4);
+    expect(z.first()).toEqual(1);
+    expect(z.uniq()).toEqual([1,2,3]);
+    expect(function(){z.last()}).toThrow();
+  });
+
+  it ("delegate with non method should throw error", function() {
+    Class.create(Delegatable, {
+        initialize: function(arr) {
+          this.arr = arr;
+          expect(
+            function(){
+              this.delegate('arr', 'length')
+            }).toThrow();
+        }
+      });
+  });
+
+  it ("#delegate_alias", function() {
+    var C = Class.create(Delegatable, {
+      initialize: function(arr) {
+        this.arr = arr;
+        this.delegate_alias('arr', ['count'], ['size']);
+      }
+    });
+
+    var z = new C([1,2,3]);
+
+    expect(z.count()).toEqual(3);
+  });
+
+  it ("expection when as count not equal alias size", function() {
+    Class.create(Delegatable, {
+        initialize: function(arr) {
+          this.arr = arr;
+          expect(
+            function(){
+              this.delegate_alias('arr', ['count', 'first'], ['size']);
+            }).toThrow();
+        }
+      });
+  });
+});
